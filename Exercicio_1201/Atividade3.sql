a307f99229e563dfc0c5d3926a987721d7ba93b0
Jéssica <jessicaasmd@gmail.com>
2022-01-12 16:33:41 -0300

    12_01

--- Exercicio_1201/Atividade3.sql
+++ Exercicio_1201/Atividade3.sql
@@ -4,22 +4,22 @@ create table tb_alunos(
 	id bigint auto_increment,
 	nome varchar(300) not null, 
 	idade int,
-    nota1 decimal not null,
-    nota2 decimal not null,
-    nota3 decimal not null,
+    nota1 (2,2)decimal not null,
+    nota2 (2,2)decimal not null,
+    nota3 (2,2)decimal not null,
     primary key (id)
     );
     
-    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values ("Pedro", 15, 8,5,9);
-    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Claudia",14,6,7,8);
-    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Roberto", 16, 4,2,8);
-    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Pablo", 15, 9,9,9);
-    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values ("Marilia", 14, 10,10,10);
-    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Josue", 15, 9,7,9);
-    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Renato", 15, 2,6,7);
-    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Maria", 15, 6,6,6);
+    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values ("Pedro", 15, 8.00,5.00,9.00);
+    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Claudia",14,6.00,7.00,8.00);
+    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Roberto", 16, 4.00,2.00,8.00);
+    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Pablo", 15, 9.00,9.00,9.00);
+    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values ("Marilia", 14, 10.00,10.00,10.00);
+    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Josue", 15, 9.00,7.00,9.00);
+    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Renato", 15, 2.00,6.00,7.00);
+    INSERT INTO tb_alunos(nome, idade, nota1, nota2, nota3) values	("Maria", 15, 6.00,6.00,6.00);
     
-    select nome,  nota3 from tb_alunos where nota3 > 7;
-	select nome,  nota3 from tb_alunos where nota3  < 7;
+    select nome,  nota3 from tb_alunos where nota3 > 7.00;
+	select nome,  nota3 from tb_alunos where nota3  < 7.00;
     
     update tb_alunos set idade = 15 where id = 5;
